2c677f4af067618dd38100d410871d93e29747bd
RubenGevorgyan <38076066+RubenGevorgyan@users.noreply.github.com>
2018-11-28 20:18:29 +0400

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -54,6 +54,63 @@ class Node:
             temp.append(root.data)
         return temp
 
+    def min_val(self,node):
+        current = node
+
+        while (current.left is not None):
+            current = current.left
+
+        return current
+
+
+    def remove(self,root, data):
+        if root is None:
+            return root
+
+        if data < root.data:
+            root.left = self.remove(root.left, data)
+
+        elif (data > root.data):
+            root.right = self.remove(root.right, data)
+
+        else:
+
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+            temp = self.min_val(root.right)
+            root.data = temp.data
+            root.right = self.remove(root.right, temp.data)
+
+        return root
+
+    def get_height(self,root):
+        if root is None:
+            return 0;
+        else:
+            left_Depth = self.get_height(root.left)
+            right_Depth = self.get_height(root.right)
+
+            if (left_Depth > right_Depth):
+                return left_Depth + 1
+            else:
+                return right_Depth + 1
+    def is_empty(self,root):
+        if root is None:
+            return False;
+        return True
+
+    def get_number_of_nodes(self,root):
+        if root is None:
+            return 0
+
+        return 1 + self.size(root.left) + self.size(root.right)
+
 def main():
     temp=int(raw_input("How many inputs do you want?"))
     root=Node(int(raw_input()))
@@ -62,8 +119,17 @@ def main():
     print("There are your inputs ")
     root.Print()
     print("\n")
-    print "These is InOrder Traverse", root.InOrder(root)
-    print "These is PreOrder Traverse", root.PreOrder(root)
-    print "These si PostOrder Traverse" , root.PostOrder(root)
+    deletion=int(raw_input("Which node do you want to delete?"))
+    root.remove(root,deletion)
+    root.Print()
+    print("\n")
+    print("Is tree empty??")
+    print("\n")
+    if(root.is_empty(root)):
+        print("The height of the tree is:::"+root.get_height(root))
+    else:
+        print("It's empty")
+    print("\n")
+    print("The number of nodes of tree is :::"+ root.get_number_of_nodes(root))
 
 main()
